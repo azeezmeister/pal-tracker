4c4c870a173bde333001dd0254745c043b507521
azeez <azeezmeister@gmail.com>
2020-01-13 15:00:14 -0700

    Pipeline  test

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -27,7 +27,7 @@ public class EnvController {
         this.cfInstanceAddress = cfInstanceAddress;
     }
 
-    @GetMapping("/env")
+    @GetMapping("/zzzenv")
     public Map<String, String> getEnv() {
         Map<String, String> env = new HashMap<>();
 
