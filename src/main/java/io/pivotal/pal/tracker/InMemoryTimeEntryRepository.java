6e4e19b7c7d866400662539ccc5ff5be0ce60d27
azeez <azeezmeister@gmail.com>
2020-01-14 12:42:08 -0700

    Spring MVC

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,50 @@
+package io.pivotal.pal.tracker;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    Map<Long, TimeEntry> map;
+    private long idSeq = 1L;
+
+    public InMemoryTimeEntryRepository() {
+        map = new HashMap<>();
+    }
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        long id = timeEntry.getId();
+        if (id == 0) {
+            timeEntry.setId(idSeq++);
+        }
+        map.put(timeEntry.getId(), timeEntry);
+        return find(timeEntry.getId());
+    }
+
+    @Override
+    public TimeEntry find(long id) {
+        return map.get(id);
+    }
+
+    @Override
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        timeEntry.setId(id);
+        if(find(timeEntry.getId()) != null) {
+            map.put(timeEntry.getId(), timeEntry);
+        }
+        return find(timeEntry.getId());
+    }
+
+    @Override
+    public List<TimeEntry> list() {
+        return map.values().stream().collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(long id) {
+        map.remove(id);
+      //  lastDeletedEntry = id;
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,58 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController {
+
+    private TimeEntryRepository timeEntryRepository;
+
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
+        TimeEntry result = timeEntryRepository.create(timeEntry);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @PutMapping(path="/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TimeEntry> update(@PathVariable Long id, @RequestBody TimeEntry timeEntry) {
+        TimeEntry result = timeEntryRepository.update(id, timeEntry);
+        if (result != null) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        timeEntryRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
+        TimeEntry result = timeEntryRepository.find(id);
+        if (result != null) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        List<TimeEntry> result = timeEntryRepository.list();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+}
